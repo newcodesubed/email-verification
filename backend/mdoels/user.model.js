0a76a0080cc31929ff234b8488af939d8abadbdb
newcodesubed <subedshahxi@gmail.com>
2025-08-17 11:04:37 +0545

    added controlles and schema

---
+++ backend/mdoels/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true},
+    password: {
+        type: String,
+        required: true},
+    isVerified: {
+        type: Boolean,
+        default: false},
+    name:{
+        type: String,
+        required: true},
+    lastlogin: {
+        type: Date,
+        default: Date.now},
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationExpiresAt: Date,
+
+},{timestamps: true})
+
