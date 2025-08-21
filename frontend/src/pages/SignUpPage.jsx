de40300d4ec547aa0358ef29f272fe0ef7be97cf
newcodesubed <subedshahxi@gmail.com>
2025-08-21 10:44:29 +0545

    feat: Inputfiled added

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,13 @@
 import{motion} from 'framer-motion'
+import Input from '../components/Input';
+import {User, Mail, Lock} from 'lucide-react';
+import { useState } from 'react';
 
 export default function SignUpPage() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleSignUP = (e) => {
         e.preventDefault();
         
@@ -16,7 +23,32 @@ export default function SignUpPage() {
             <h2 className='text-3xl font-bold text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text mb-6'>
             Create Account
         </h2>
-        <form onSubmit={handleSignUP}></form>
+        <form onSubmit={handleSignUP}>
+            <Input
+                icon={User}
+                type="text"
+                placeholder="Full Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+            />
+            <Input
+                icon={Mail}
+                type="email"
+                placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            <Input
+                icon={Lock}
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+        </form>
         </div>
     </motion.div>
   )
