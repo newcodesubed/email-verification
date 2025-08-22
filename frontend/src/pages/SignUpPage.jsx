4a60fd24d149fa8bb00fc617ebcde6367bef8665
newcodesubed <subedshahxi@gmail.com>
2025-08-22 12:55:13 +0545

    signup page update

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import{motion} from 'framer-motion'
 import Input from '../components/Input';
 import {User, Mail, Lock} from 'lucide-react';
 import { useState } from 'react';
-
+import {Link} from "react-router-dom"
 export default function SignUpPage() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -48,7 +48,27 @@ export default function SignUpPage() {
                 onChange={(e) => setPassword(e.target.value)}
                 required
             />
+            <motion.button
+                type="submit"
+className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+						font-bold rounded-lg shadow-lg hover:from-green-600
+						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+						 focus:ring-offset-gray-900 transition duration-200'                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}>
+                    Sign Up
+            </motion.button>
+
+
         </form>
+        </div>
+        <div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center rounded-b-2xl'>
+            <p className='text-sm text-gray-400'>
+                Already have an account? 
+                <Link to={"/login"} className='text-green-400 hover:text-green-800 ml-1'>
+                    Log In
+                </Link>
+            </p>
+
         </div>
     </motion.div>
   )
