63f740c3ccc049fcac0169e09021609ef7a89f48
newcodesubed <subedshahxi@gmail.com>
2025-08-22 13:32:33 +0545

    state manage for login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Mail, Lock, Loader } from "lucide-react";
+import { Link } from "react-router-dom";
 
-function LoginPage() {
+function  LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+		e.preventDefault();
+		
+	};
+
+
   return (
-    <div>LoginPage</div>
+    <motion.div
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.5 }}
+			className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
+		></motion.div>
   )
 }
 
