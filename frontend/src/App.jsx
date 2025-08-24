8eb4fac30eaf2fbafc8a0278f450a5ffe565ddd9
newcodesubed <subedshahxi@gmail.com>
2025-08-24 15:32:36 +0545

    feat: chechking auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
 import FloatingShap from './components/FloatingShap'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
+
 import { Route, Routes } from 'react-router-dom'
 import { EmailVerificationPage } from './pages/EmailVerificationPage'
 import {Toaster} from 'react-hot-toast'
+import { useAuthStore } from './store/authStore'
+import { useEffect } from 'react'
+
 export default function App() {
+
+  const {isCheckingAuth, checkAuth, isAuthenticated, user}= useAuthStore();
+
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth])
+
+  console.log("isauthenticated", isAuthenticated);
+  console.log("user", user);
+
+
   return (
     <div className='min-h-screen bg-gradient-to-br
     from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
