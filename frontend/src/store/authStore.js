962c9adf4172058bee6ccc43854032d3afb32a78
newcodesubed <subedshahxi@gmail.com>
2025-08-24 13:49:53 +0545

    feat: verify email store added

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,5 +1,6 @@
 import {create} from "zustand";
 import axios from "axios";
+import { response } from "express";
 
 const API_URL = "http://localhost:5000/api/auth"
 axios.defaults.withCredentials = true;
@@ -26,6 +27,22 @@ export const useAuthStore = create((set) => ({
         });
         throw error;
         
+    }
+  },
+
+  verifyEmail: async (code)=>{
+    set({isLoading:true, error:null});
+    try {
+        const response = await axios.post(`${API_URL}/verify-email`,{code});
+        set({
+            user:response.data.user,
+            isAuthenticated:true,
+            isLoading:false,
+        });
+        return response.data;
+    } catch (error) {
+        set({error: error.response.data.message || "Error verifying email", isLoading:false});
+        throw error;
     }
   }
 
